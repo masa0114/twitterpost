0b42341eea1775a71a5ace6a472ac0a092609c2d
masahiro.kimura <masahiro.kimura@10.52.10.25>
2015-11-09 11:34:03 +0900

    Twitterテスト投稿処理

---
+++ twitterpost.php
@@ -0,0 +1,20 @@
+<?php
+
+// OAuthライブラリの読み込み
+require "twitteroauth/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+//認証情報４つ
+$consumerKey = "fnmUm8h8VExeY2DulXhe2Yzqh";
+$consumerSecret = "t17aBKftnPTmnOYeAV7mVY32PXZC0XW2t4h28QyyhGpSadDGqb";
+$accessToken = "57595798-LUSxa2RpRKygnC78L309zhkuZZZ3Nna2VJxQoKV1V";
+$accessTokenSecret = "4UbFP1Bo1HsNHdL4KmRdgCBCHw1iNotDz4W9jkGtbD2wH";
+
+//接続
+$connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+
+//ツイート
+$res = $connection->post("statuses/update", array("status" => "テストテストメッセージ"));
+
+//レスポンス確認
+var_dump($res);
